6306663dceae8aa48dc760775c8d51a6d15fc587
sooboi <dlgbrkd123@gmail.com>
2023-08-21 15:58:09 +0900

    Ts : 타입스크립트 원시 타입과 리터럴 타입

---
+++ Ts/section2/src/chapter1.ts
@@ -0,0 +1,32 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = -0.123;
+let num4: number = Infinity;
+let num5: number = -Infinity;
+let num6: number = NaN;
+
+// String
+let str1: string = "hello";
+let str2: string = "hello";
+let str3: string = `hello`;
+let str4: string = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// "strictNullChecks" 옵션으로 null 빡세게 체크
+// let numA: number = null;
+
+// 리터럴
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
+// 원시타입에 포함되는 값 중 하나를 타입인것처럼 사용
