bdedcbdce13b7ce0fc0ca2f32383bf323c74d0de
sooboi <dlgbrkd123@gmail.com>
2023-08-25 21:20:58 +0900

    Ts-React : 리액트에서 타입스크립트로 투두 만들기

--- React/ts-react/src/App.tsx
+++ React/ts-react/src/App.tsx
@@ -1,7 +1,98 @@
+import React, {
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 import "./App.css";
+import Editor from "./component/Editor";
+import { Todo } from "./types";
+import TodoItem from "./component/TodoItem";
+
+// reducer 에 사용할 Action 정의
+type Action =
+  | {
+      type: "CREATE";
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | { type: "REMOVE"; id: number };
+
+// reducer 생성
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [...state, action.data];
+    }
+    case "REMOVE": {
+      return state.filter((it) => it.id !== action.id);
+    }
+  }
+}
+
+// Context 로 todo state 와 상태 변화 함수들을 전역에 공급
+export const TodoStateContext = React.createContext<Todo[] | null>(null);
+export const TodoDispatchContext = React.createContext<{
+  onClickAdd: (text: string) => void;
+  onClickRemove: (id: number) => void;
+} | null>(null);
+
+export function useTodoDispatch() {
+  const dispatch = useContext(TodoDispatchContext);
+  if (!dispatch) throw new Error("TodoDispatchContext에 문제 발생");
+  return dispatch;
+}
 
 function App() {
-  return <div className="App"></div>;
+  // reducer 상태관리
+  const [todo, dispatch] = useReducer(reducer, []);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        content: text,
+      },
+    });
+  };
+
+  const onClickRemove = (id: number) => {
+    dispatch({
+      type: "REMOVE",
+      id: id,
+    });
+  };
+
+  useEffect(() => {
+    console.log(todo);
+  }, [todo]);
+
+  return (
+    <div className="App">
+      <TodoStateContext.Provider value={todo}>
+        <TodoDispatchContext.Provider
+          value={{
+            onClickAdd,
+            onClickRemove,
+          }}
+        >
+          <h1>Todo</h1>
+          <Editor />
+          <div>
+            {todo.map((todo) => (
+              <TodoItem key={todo.id} {...todo} />
+            ))}
+          </div>
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
+    </div>
+  );
 }
 
 export default App;
---
+++ React/ts-react/src/component/Editor.tsx
@@ -0,0 +1,26 @@
+import { ReactElement, useState } from "react";
+import { TodoDispatchContext, useTodoDispatch } from "../App";
+
+interface Props {}
+
+export default function Editor(props: Props) {
+  const dispatch = useTodoDispatch();
+
+  const [text, setText] = useState("");
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const onClickButton = () => {
+    dispatch.onClickAdd(text);
+    setText("");
+  };
+
+  return (
+    <div className="Editor">
+      <input value={text} onChange={onChangeInput} />
+      <button onClick={onClickButton}>추가</button>
+    </div>
+  );
+}
