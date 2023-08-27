08246f4c2a1c8ed1736de74f349ff027512b79bb
sooboi <dlgbrkd123@gmail.com>
2023-08-28 01:09:26 +0900

    React : 로딩, 에러 상태 추가

--- React/basic-props/src/basic/components/Products.jsx
+++ React/basic-props/src/basic/components/Products.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
@@ -14,15 +16,23 @@ export default function Products() {
   };
 
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
     fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
-      });
+      })
+      .catch((error) => setError("에러 발생 비상 !"))
+      .finally(() => setLoading(false));
+
     return () => {
       console.log("언마운트");
     };
   }, [checked]);
+
+  if (loading) return <p>Loading . . .</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <div className="Products">
--- React/basic-props/src/index.js
+++ React/basic-props/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppMentorsUp from "./basic/AppMentorsUp";
+import AppProduct from "./basic/AppProduct";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentorsUp />
+    <AppProduct />
   </React.StrictMode>
 );
