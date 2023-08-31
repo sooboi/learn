26700dde1c7af46e3216331480e9b8a3faa32cb4
sooboi <dlgbrkd123@gmail.com>
2023-08-31 23:07:57 +0900

    React : React router-dom, Outlet, useParams

---
+++ React/router/src/App.js
@@ -0,0 +1,35 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import Videos from "./pages/Videos";
+import Root from "./pages/Root";
+import VideoDetail from "./pages/VideoDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/videos",
+        element: <Videos />,
+      },
+      {
+        path: "/videos/:videoId",
+        element: <VideoDetail />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
